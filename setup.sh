d8dc17ae9bb7696b53528fee37708bfe089f19c7
Brian Zalewski <TimeTravelersHackMe@users.noreply.github.com>
2015-09-19 22:19:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -218,6 +218,8 @@ execCommand "sudo mv wp-cli.phar /usr/local/bin/wp"
 outputMessage "Installing NVM"
 execCommand "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash"
 execCommand ". $HOME/.nvm/nvm.sh"
+# Above command not registering so trying without execCommand wrapper
+. $HOME/.nvm/nvm.sh
 execCommand "nvm install stable"
 execCommand "n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local"
 
