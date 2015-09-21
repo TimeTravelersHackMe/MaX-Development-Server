71fe9ed1fe82b9188d663bd24183e586b1482a5c
Brian Zalewski <TimeTravelersHackMe@users.noreply.github.com>
2015-09-21 14:46:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -297,7 +297,7 @@ execCommand "service tomcat8 start"
 
 # Install PostgreSQL
 outputMessage 'Installing PostgreSQL'
-execCommand "apt-get install -y postgresql postgresql-contrib"
+execCommand "apt-get install -y postgresql postgresql-contrib php5-pgsql"
 
 # Install RVM (Ruby Version Manager)
 # Source: https://rvm.io/rvm/install
